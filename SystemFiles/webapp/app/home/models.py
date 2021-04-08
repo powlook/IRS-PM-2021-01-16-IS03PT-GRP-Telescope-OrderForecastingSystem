6ed2878bbc1d72cc05ddd0276bec5f08e3b7dae2
Nicole Tan <ntan804@gmail.com>
2021-04-08 22:31:37 +0800

    Initial Chart

--- SystemFiles/webapp/app/home/models.py
+++ SystemFiles/webapp/app/home/models.py
@@ -3,7 +3,7 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from sqlalchemy import Binary, Column, Integer, String, DateTime, Float, Text
+from sqlalchemy import Binary, Column, Integer, String, DateTime, Float, Text, text
 from sqlalchemy.inspection import inspect
 
 from app import db
@@ -31,3 +31,44 @@ class Order(db.Model, Serializer):
     def serialize(self):
         d = Serializer.serialize(self)
         return d
+
+class OrderProduct(db.Model, Serializer):
+    __tablename__ = 'orders_products_v2'
+
+    product_sku =  Column(Text, primary_key=True)
+    product_quantity =  Column(Integer)
+    order_id =  Column(Integer)
+    order_date =  Column(DateTime, primary_key=True)
+
+    def __repr__(self):
+        return str(self.product_sku + '_' + str(self.order_id))
+
+    def serialize(self):
+        d = Serializer.serialize(self)
+        return d
+
+    def group_by_product():
+        query = text("SELECT DATE_FORMAT(order_date, '%Y-%m') AS date, product_sku, SUM(product_quantity) AS quantity FROM orders_products_v2 WHERE order_date >= '2020-01-01' and order_date <= '2020-12-31' and product_sku IN (SELECT DISTINCT(item) FROM top20forecasts_ARIMA) GROUP BY `date`, product_sku")
+        d = db.engine.execute(query)
+        return d
+
+class ForecastArima(db.Model, Serializer):
+    __tablename__ = 'top20forecasts_ARIMA'
+
+    month = Column(Text, primary_key=True)
+    test = Column(Integer)
+    predict = Column(Integer)
+    item = Column(Text, primary_key=True)
+
+    def __repr__(self):
+        return str(self.month + '_' + self.item)
+
+    def serialize(self):
+        d = Serializer.serialize(self)
+        return d
+
+    def format():
+        query = text("SELECT * from top20forecasts_ARIMA LIMIT :limit")
+        d = db.engine.execute(query, limit=5)
+        return d
+
--- SystemFiles/webapp/app/home/routes.py
+++ SystemFiles/webapp/app/home/routes.py
@@ -4,11 +4,12 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from app.home import blueprint
-from app.home.models import Order
+from app.home.models import Order, OrderProduct, ForecastArima
 from flask import render_template, redirect, url_for, request, jsonify
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
+import sqlalchemy as sa
 
 @blueprint.route('/index')
 @login_required
@@ -38,8 +39,34 @@ def inventory():
 @blueprint.route('/smart-procurement', methods=['GET'])
 @login_required
 def smart_procurement():
-    orders = Order.query.limit(5).all()
-    return render_template( 'smart-procurement.html', orders=orders, segment='smart-procurement')
+    forecasts = ForecastArima.query.all()
+    orderproducts = OrderProduct.group_by_product()
+
+    dates = []
+    skus = []
+    original = []
+    for op in orderproducts:
+        dates.append(op.date)
+        skus.append(op.product_sku)
+        original.append(op)
+
+    dates = sorted(list(set(dates)))
+    skus = sorted(list(set(skus)))
+
+    emptyList = [0] * len(dates)
+    productDict = dict.fromkeys(skus, [0] * len(dates))
+
+    for ab in original:
+        try:
+            index = dates.index(ab.date)
+            curr_arr = productDict[ab.product_sku].copy()
+            curr_arr[index] = int(ab.quantity)
+            sku = ab.product_sku
+            productDict.update({ sku: curr_arr })
+        except:
+            return
+
+    return render_template( 'smart-procurement.html', forecasts=forecasts, productdict=productDict, dates=dates, segment='smart-procurement')
 
 
 ### API
